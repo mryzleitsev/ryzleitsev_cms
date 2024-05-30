e6ab562eba81b152ab019c59f79fe1094088595a
X1atle <myron.ryzleitsev@gmail.com>
2024-05-30 05:02:19 +0200

    Cards added to dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,18 @@
+import Card from "../ui/dashboard/card/card";
+import styles from "../ui/dashboard/dashboard.module.css";
+
 const Dashbord = () => {
     return (
-        <div>Dashboard</div>
-    )
+        <div className={styles.wrapper}>
+            <div className={styles.main}>
+                <div className={styles.cards}>
+                    <Card></Card>
+                    <Card></Card>
+                    <Card></Card>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Dashbord;
